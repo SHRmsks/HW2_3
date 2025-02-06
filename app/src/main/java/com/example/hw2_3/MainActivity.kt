08ace4771889244a91aa1779f0cc3801f94f9fe5
Haoran <suhr08@bu.edu>
2025-02-05 23:01:02 -0500

    add the pause logic and icon

--- app/src/main/java/com/example/hw2_3/MainActivity.kt
+++ app/src/main/java/com/example/hw2_3/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.hw2_3
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,9 +46,10 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun musicPlayer(music: String) {
+        var clicked by rememberSaveable { mutableStateOf(false) }
         Box(
             modifier =
-                Modifier.fillMaxWidth(0.7f).height(300.dp).background(
+                Modifier.fillMaxWidth(0.7f).height(300.dp).clip(RoundedCornerShape(10.dp)).background(
                     brush =
                         Brush.linearGradient(
                             colors =
@@ -68,7 +73,7 @@ class MainActivity : ComponentActivity() {
                     Image(
                         painter = painterResource(id = R.drawable.mus),
                         contentDescription = "Jay Chou",
-                        modifier = Modifier.height(150.dp).width(150.dp),
+                        modifier = Modifier.height(150.dp).width(150.dp).clip(RoundedCornerShape(10.dp)),
                     )
                     Text(text = music, modifier = Modifier, softWrap = true, color = Color(0xFFd0f2f2), fontSize = 15.sp)
                 }
@@ -77,31 +82,39 @@ class MainActivity : ComponentActivity() {
                     horizontalArrangement = Arrangement.SpaceAround,
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Button (onClick = { Unit} ){
-                    Image(
-                        painter = painterResource(R.drawable.previous),
-                        contentDescription = "previous",
-                        modifier = Modifier.height(20.dp).width(20.dp),
-                    )
-                }
+                    TextButton(onClick = { Unit }) {
+                        Image(
+                            painter = painterResource(R.drawable.previous),
+                            contentDescription = "previous",
+                            modifier = Modifier.height(20.dp).width(20.dp),
+                        )
+                    }
                     Spacer(modifier = Modifier.width(20.dp))
-                    Button (onClick = { Unit} ){
-                    Image(
-                        painter = painterResource(R.drawable.play),
-                        contentDescription = "Play",
-                        modifier = Modifier.height(30.dp).width(30.dp),)
+                    TextButton(onClick = { clicked = !clicked}) {
+                        if (clicked) {
+                            Image(
+                                painter = painterResource(R.drawable.pause),
+                                contentDescription = "Pause",
+                                modifier = Modifier.height(40.dp).width(40.dp),
+                            )
+                        } else {
+                            Image(
+                                painter = painterResource(R.drawable.play),
+                                contentDescription = "Play",
+                                modifier = Modifier.height(30.dp).width(30.dp),
+                            )
+                        }
+                    }
 
+                    Spacer(modifier = Modifier.width(20.dp))
+                    TextButton(onClick = { Unit }) {
+                        Image(
+                            painter = painterResource(R.drawable.next),
+                            contentDescription = "Next",
+                            modifier = Modifier.height(20.dp).width(20.dp),
+                        )
+                    }
                 }
-
-                Spacer(modifier = Modifier.width(20.dp))
-                Button (onClick = { Unit} ){
-                    Image(
-                        painter = painterResource(R.drawable.next),
-                        contentDescription = "Next",
-                        modifier = Modifier.height(20.dp).width(20.dp),)
-
-                }
-            }
             }
         }
     }
